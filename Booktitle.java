37ee201212f499dca36bd99a62b62a7414e6f3c9
mohammed furkhan ahmed <68239187+mohammedfurkhan@users.noreply.github.com>
2020-10-09 06:22:00 -0700

    Add files via upload

---
+++ Booktitle.java
@@ -0,0 +1,85 @@
+public class Booktitle {
+
+    String[] cse_1_1_BookTile ={"MATHEMATICS- I","ENGINEERING CHEMISTRY","ENGINEERING PHYSICS - I","PROFESSIONAL COMMUNICATION IN ENGLISH","ENGINEERING MECHANICS","BASIC ELECTRICAL AND ELECTRONICS ENGINEERING"};
+    String[] cse_1_1_BookAuthor = {"Dennis G. Zill","P.C. Jain and M. Jain","Halliday, Resnick and Kramer John wiley and Sons","","K. Vijaya Kumar Reddy","M S Sukija TK Nagasarkar"};
+    String[] cse_1_1_Edition ={"","","Edition 4","","",""};
+    String[] cse_1_1_Publications ={"Cengage Learning publishers","Dhanpatrai Publishing Company","","","","Oxford University"};
+    String[] cse_1_1_UrlLink ={""};
+
+
+    String[] cse_1_2_BookTile ={"ENGINEERING PHYSICS-II","MATHEMATICS-II","MATHEMATICS-III","COMPUTER PROGRAMMING IN C","ENGINEERING GRAPHICS"};
+    String[] cse_1_2_BookAuthor = {"A.J. Dekkar","R.K.Jain & SRK Iyengar"," Richard Arnold Johnson, Irwin Miller and John E. Freund"," B. A. Forouzan and R. F. Gilberg"," Basant Agrawal,Mc Agrawal & Mc Graw Hill"};
+    String[] cse_1_2_Edition ={"","","","Third Edition",""};
+    String[] cse_1_2_Publications ={"Macmillan publishers Ind. Ltd","Cengage LearningPublishers","","Cengage Learning",""};
+    String[] cse_1_2_UrlLink ={""};
+
+
+    String[] cse_2_1_BookTile ={"MATHEMATICS – IV","DATA STRUCTURE THROUGH C++","MATHEMATICS FOUNDATION OF COMPUTER SCIENCE","DIGITAL LOGIC DESIGN","OBJECT ORIENTED PROGRAMMING THROUGH JAVA"};
+    String[] cse_2_1_BookAuthor = {"Dennis G. Zill","Sartaj Sahni","J.P.Tremblay, R. Manohar","M. Morris Mano, M.D.Ciletti","Herbert Schildt"};
+    String[] cse_2_1_Edition ={"","2nd Edition","","5th edition","9th edition"};
+    String[] cse_2_1_Publications ={"Johns and Bartlett Publishers","Universities Press","McGraw Hill education (India) Private Limited","Pearson","McGraw Hill Education(India) Pvt. Ltd"};
+    String[] cse_2_1_UrlLink ={"https://mariosuazo.files.wordpress.com/2013/08/dennis_zill_a_first_course_in_complex_analysis_wbookfi-org.pdf",
+                                "http://www.musaliarcollege.com/e-Books/CSE/Data%20structures%20algorithms%20and%20applications%20in%20C.pdf",
+                                "http://discrete.openmathbooks.org/pdfs/dmoi-tablet.pdf",
+                                "http://www.portcity.edu.bd/files/636444791235373856_Digitallogicdesign.pdf",
+                                  "https://x-file.oss-cn-beijing.aliyuncs.com/it-books/Java_8_The_Complete_Reference_Ninth_Edition.pdf"};
+
+
+    String[] cse_2_2_BookTile ={"COMPUTER ORGANIZATION","DATABASE MANAGEMENT SYSTEMS","OPERATING SYSTEMS","FORMAL LANGUAGES AND AUTOMATA THEORY","BUSINESS ECONOMICS AND FINANCIAL ANALYSIS"};
+    String[] cse_2_2_BookAuthor = {"M. Moris Mano","Raghu Ramakrishnan, Johannes Gehrke","Abraham Silberschatz, Peter B. Galvin, Greg Gagne","John E.Hopcroft, Rajeev Motwani, Jeffrey D. Ullman","D. D. Chaturvedi, S. L. Gupta"};
+    String[] cse_2_2_Edition ={"3rd Edition","3rd Edition","9th Edition","3rd Edition",""};
+    String[] cse_2_2_Publications ={"Pearson","McGraw Hill Education (India) Private Limited","","Pearson Education","International Book House Pvt. Ltd. 2013"};
+    String[] cse_2_2_UrlLink ={"https://www.booksfree.org/computer-system-architecture-morris-mano-third-edition-pdf/",
+                                "https://docplayer.net/1094427-Database-management-systems-solutions-manual-third-edition.html",
+                                 "https://www.pdf-archive.com/2016/12/25/operating-system-concepts-9th-edition/operating-system-concepts-9th-edition.pdf",
+                                    "http://ce.sharif.edu/courses/94-95/1/ce414-2/resources/root/Text%20Books/Automata/John%20E.%20Hopcroft,%20Rajeev%20Motwani,%20Jeffrey%20D.%20Ullman-Introduction%20to%20Automata%20Theory,%20Languages,%20and%20Computations-Prentice%20Hall%20(2006).pdf",
+                                      "http://elibrary.vssdcollege.ac.in/web/data/books-com-sc/bcom-1/BUSINESS%20ECONOMICS.pdf"};
+
+
+
+    String[] cse_3_1_BookTile ={"DESIGN AND ANALYSIS OF ALGORITHM","FUNDAMENTALS OF MANAGEMENT","SOFTWARE ENGINEERING","DATA COMMUNICATION AND COMPUTER NETWORK","Environmental Impact Assessment"};
+    String[] cse_3_1_BookAuthor = {"P.H.Dave,H.B.Dave","Stephen P Robbins","Roger S Pressman","Behrouz A.Fourouzan",""};
+    String[] cse_3_1_Edition ={"Second Edition","","Sixth Edition","Fourth Edition",""};
+    String[] cse_3_1_Publications ={"Universities Press","Cengage Learning","McGraw Hill International Edition","",""};
+    String[] cse_3_1_UrlLink ={"https://doc.lagout.org/science/0_Computer%20Science/2_Algorithms/The%20Design%20and%20Analysis%20of%20Computer%20Algorithms%20%5BAho%2C%20Hopcroft%20%26%20Ullman%201974-01-11%5D.pdf",
+                                "http://www.mim.ac.mw/books/Fundamentals%20of%20Management.pdf",
+                                "http://ce.sharif.edu/courses/98-99/2/ce474-2/resources/root/Roger%20S.%20Pressman_%20Bruce%20R.%20Maxin%20-%20Software%20Engineering_%20A%20Practitioner%E2%80%99s%20Approach-McGraw-Hill%20Education%20(2014).pdf",
+                                 "http://zai.lecturer.pens.ac.id/Kuliah/Komunikasi%20Data/Buku%20Referensi/Data%20Communications%20and%20Networking%20By%20Behrouz%20A.Forouzan.pdf",
+                                  "",};
+
+
+    String[] cse_3_2_BookTile ={"COMPILER DESIGN","WEB TECHNOLOGY","CRYTOGRAPHY AND NETWORK SECURITY","DESIGN PATTERN","INTELLECTUAL PROPERTY RIGHTS"};
+    String[] cse_3_2_BookAuthor = {"Alfred V.Aho, Monica S.Lam, Jeffry D. Ullman","Uttamm K Roy","William Stallings","Erich Gamma",""};
+    String[] cse_3_2_Edition ={"Second Edition","","Sixth Edition","",""};
+    String[] cse_3_2_Publications ={"Pearson","Oxford University Press","Pearson Education","Pearson Education"};
+    String[] cse_3_2_UrlLink ={"http://ce.sharif.edu/courses/94-95/1/ce414-2/resources/root/Text%20Books/Compiler%20Design/Alfred%20V.%20Aho,%20Monica%20S.%20Lam,%20Ravi%20Sethi,%20Jeffrey%20D.%20Ullman-Compilers%20-%20Principles,%20Techniques,%20and%20Tools-Pearson_Addison%20Wesley%20(2006).pdf",
+                                "https://mrcet.com/downloads/digital_notes/CSE/III%20Year/Web%20Technologies.pdf",
+                                 "https://uomustansiriyah.edu.iq/media/lectures/6/6_2017_03_17!10_56_57_PM.pdf",
+                                  "http://www.uml.org.cn/c++/pdf/designpatterns.pdf",""};
+
+
+
+
+
+    String[] cse_4_1_BookTile ={"DATA MINING","PRINCIPLES OF PROGRAMMING LANGUAGE","PYTHON PROGRAMMING","SOFTWARE PROCESS AND PROCESS MANAGEMENT","BLOCKCHAIN TECHNOLOGY"};
+    String[] cse_4_1_BookAuthor = {"Morgan Kaufmann","Robert .W. Sebesta","Wesley J. Chun","Watts S. Humphrey","Melanie Swan"};
+    String[] cse_4_1_Edition ={"Second Edition","10th edition","Second Edition","",""};
+    String[] cse_4_1_Publications ={"MorganKaufmann Publishers","Pearson Education","Pearson","Pearson Education",""};
+    String[] cse_4_1_UrlLink ={"http://ccs1.hnue.edu.vn/hungtd/DM2012/DataMining_BOOK.pdf",
+                       "https://docs.google.com/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnxlbmdoZXNoYW1hYmRlbHNhbGFtc2l0ZXxneDo2MzRkZDA2Zjc1ZDY3YzIx",
+                       "http://emixam.sevla.free.fr/books/2.PythoProg_softarchive.net.pdf",
+                        "https://www.edutechlearners.com/download/Software%20Project%20Management.pdf",
+                        "http://book.itep.ru/depository/blockchain/blockchain-by-melanie-swan.pdf"};
+
+
+    String[] cse_4_2_BookTile ={""};
+    String[] cse_4_2_BookAuthor = {""};
+    String[] cse_4_2_Edition ={""};
+    String[] cse_4_2_Publications ={""};
+    String[] cse_4_2_UrlLink ={""};
+
+
+
+
+
+}
